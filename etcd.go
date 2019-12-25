0e4c6ffb1ad6a22402aa62c8210eab6d624edf4f
liangyuzhou <liangyuzhou@tencent.com>
2019-12-25 17:56:17 +0800

    feat: update etcd to go.etcd.io/etcd

--- etcd.go
+++ etcd.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"fmt"
 	pathutil "path"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/coreos/etcd/clientv3"
-
-	"github.com/coreos/etcd/pkg/transport"
-	"strconv"
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/pkg/transport"
 )
 
 const DefaultPrefix = "/chart_backend_bucket"
